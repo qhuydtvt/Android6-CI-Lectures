9599a29325a2bc08d8eb8d93dddaaa49302857e8
qhuydtvt <qhuydtvt@gmail.com>
2016-10-18 20:58:33 +0700

    Session 6

---
+++ Session6/src/controllers/EnemyPlaneControllerManager.java
@@ -0,0 +1,37 @@
+package controllers;
+
+import models.EnemyPlane;
+import utils.Utils;
+import views.GameView;
+
+/**
+ * Created by apple on 10/11/16.
+ */
+public class EnemyPlaneControllerManager extends ControllerManager {
+
+    public EnemyPlaneControllerManager() {
+        super();
+
+        for(int i = 0; i < 10; i++) {
+            int y = 60;
+            int x = i * (EnemyPlane.ENEMY_PLANE_WIDTH + 5);
+            EnemyPlaneController enemyPlaneController = new EnemyPlaneController(
+                    new EnemyPlane(x, y),
+                    new GameView(Utils.loadImageFromRes("plane1.png")),
+                    new DownFlyBehavior(1),
+                    new DownShootBehavior());
+            baseControllers.add(enemyPlaneController);
+        }
+
+        for(int i = 0; i < 10; i++) {
+            int y = 100;
+            int x = i * (EnemyPlane.ENEMY_PLANE_WIDTH + 5);
+            EnemyPlaneController enemyPlaneController = new EnemyPlaneController(
+                    new EnemyPlane(x, y),
+                    new GameView(Utils.loadImageFromRes("plane1.png")),
+                    new DownRightFlyBehavior(),
+                    null);
+            baseControllers.add(enemyPlaneController);
+        }
+    }
+}
