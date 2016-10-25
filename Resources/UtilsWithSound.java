536865e3d522e8592ade82f381103e45be967899
qhuydtvt <qhuydtvt@gmail.com>
2016-10-25 18:54:30 +0700

    Added Sound playing

---
+++ Resources/UtilsWithSound.java
@@ -0,0 +1,38 @@
+package vn.edu.techkids;
+
+import javax.imageio.ImageIO;
+import javax.sound.sampled.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Vector;
+
+/**
+ * Created by qhuydtvt on 5/13/2016.
+ */
+public class Utils {
+
+    public static void playSound(String audioUrl, boolean repeat) {
+
+        File soundFile = new File(audioUrl);
+        try {
+            AudioInputStream audioIn = AudioSystem.getAudioInputStream(soundFile);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioIn);
+            clip.start();
+            if(repeat) {
+                clip.loop(Clip.LOOP_CONTINUOUSLY);
+            }
+             else {
+                clip.loop(0);
+            }
+        } catch (UnsupportedAudioFileException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (LineUnavailableException e) {
+            e.printStackTrace();
+        }
+    }
+}
